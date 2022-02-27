51d067bce13f5ec19cbc47660ec560d019b7e27f
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-27 14:17:50 -0600

    sprite

--- ship.py
+++ ship.py
@@ -2,12 +2,14 @@
 
 
 import pygame 
+from pygame.sprite import Sprite 
 
 class Ship: 
     """A class to manage the ship."""
 
     def __init__(self,ai_game):
         """Initialize the ship and set its starting position"""
+        super().__init__() #displaying the number of ships 
         #assign the screen to an attribute of ship, so we can access it easily in all the methods in the class 
         self.screen = ai_game.screen 
         self.settings = ai_game.settings 
