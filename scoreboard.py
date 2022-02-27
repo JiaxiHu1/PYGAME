7e8d72c51d1df301eea3d66532aa2247a7e8d193
Jiaxi Hu <Jiaxi_Hu1@baylor.edu>
2022-02-27 14:08:20 -0600

    prep

--- scoreboard.py
+++ scoreboard.py
@@ -19,6 +19,7 @@ class ScoreBoard:
 
         # prepare the initial score image 
         self.prep_score()
+        self.prep_high_score() #the high score will be displayed separately from the score, so we need a new method 
 
     def prep_score(self):
         """Turn the score into a rendered image"""
@@ -35,3 +36,14 @@ class ScoreBoard:
     def show_score(self):
         """draw score to the screen"""
         self.screen.blit(self.score_image,self.score_rect)
+
+    def prep_high_score(self):
+        """turn the high score into a rendered image"""
+        rounded_high_score = round(self.stats.high_score, -1)
+        high_score_str = "highest score: " + "{:,}".format(rounded_high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # center the high score at the top of the screen 
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
